ce8b0e113613e67953b81bd3cd60a5e010f672ba
Awais Muzaffar <mail@awaismuzaffar.com>
2018-03-08 14:34:20 +0000

    created very basic components

---
+++ src/components/ObjectivesStatusTabs/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ObjectivesStatusTabs = (props) => 
+    <div>
+        <ul>
+            <li>{"Drafts."}</li>
+            <li>{"In Progress."}</li>
+            <li>{"Complete."}</li>
+            <li>{"Show all."}</li>
+        </ul>
+    </div>
+
+export default ObjectivesStatusTabs
