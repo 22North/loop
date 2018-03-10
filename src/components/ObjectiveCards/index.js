68ab207b122c464d0a4e47909f5714861cbf3724
Awais Muzaffar <mail@awaismuzaffar.com>
2018-03-10 17:38:40 +0000

    filter objectives by type / improved layout and ux of objectives page

--- src/components/ObjectiveCards/index.js
+++ src/components/ObjectiveCards/index.js
@@ -8,17 +8,25 @@ class ObjectiveCards extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            objectiveType: 'all'
+            objectiveType: null
         };
+    }
+
+    setObjectiveType(type) {
+        this.setState({objectiveType: type})
+    }
+
+    isVisible(type) {
+        return (this.state.objectiveType === type || this.state.objectiveType === null ? true : false)
     }
     
     render() {
         return (    
             <div>
-                <ObjectiveCardsFilter />
-                <ObjectiveCardsByDraft data={this.props.data} />
-                <ObjectiveCardsByInProgress />
-                <ObjectiveCardsByComplete />
+                <ObjectiveCardsFilter setType={ this.setObjectiveType.bind(this) } />
+                <ObjectiveCardsByDraft data={ this.props.data.objectives.filter(item => item.type === 'draft') } isVisible={ this.isVisible('draft') } />
+                <ObjectiveCardsByInProgress data={ this.props.data.objectives.filter(item => item.type === 'inProgress') } isVisible={ this.isVisible('inProgress') } />
+                <ObjectiveCardsByComplete data={ this.props.data.objectives.filter(item => item.type === 'complete') } isVisible={ this.isVisible('complete') } />
             </div>
         )
     }
