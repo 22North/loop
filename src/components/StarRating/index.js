ec63a2a2eddb6b959bda91e264c57eeeed0d464f
Awais Muzaffar <mail@awaismuzaffar.com>
2018-04-27 18:22:21 +0100

    feedback page set up

---
+++ src/components/StarRating/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './style.css'
+
+class StarRating extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    onClick(rating) {
+        this.props.onClick(rating)
+    }
+
+    render() {
+        return (
+            <div>
+                <span onClick={() => this.onClick(1)}>star 1 </span>
+                <span onClick={() => this.onClick(2)}>star 2 </span>
+                <span onClick={() => this.onClick(3)}>star 3 </span>
+                <span onClick={() => this.onClick(4)}>star 4 </span>
+                <span onClick={() => this.onClick(5)}>star 5 </span>
+            </div>
+        )
+    }
+}
+
+export default StarRating
--- src/containers/Feedback/index.js
+++ src/containers/Feedback/index.js
@@ -1,14 +1,52 @@
+import './style.css'
 import React from 'react'
+import StarRating from '../../components/StarRating'
 
-const Feedback = () => 
-    <div className="main-container main-container--feedback">
-        <div className="container">
-            <div className="row">
-                <div className="col-sm-12">
-                    <h2 className="main-heading">{"General Feedback."}</h2>
+class Feedback extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedUser: null,
+            selectedRating: null
+        };
+    }
+
+    onStarRatingClick(rating) {
+        this.setState({...this.state, selectedRating: rating})
+    }
+
+    render() {
+        return (
+            <div className="main-container main-container--feedback">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <h2 className="main-heading">{"General Feedback."}</h2>
+                        </div>
+                    </div>   
+
+                    <div className="row">
+                        <div className="col-sm-12">
+
+                            <div className="general-feedback-form">
+                                <form>
+                                    Your feedback.
+                                    <p>Let them know what they should stop/start/continue to do.</p>
+                                    <textarea placeholder="Comments..."></textarea>
+                                    <p>How likely are you to recommend them out of 5.</p>
+                                    <StarRating onClick={this.onStarRatingClick.bind(this)} />
+                                    <button className="btn">Send.</button>
+                                </form>
+                            </div>
+
+                            
+                        </div>
+                    </div>
                 </div>
-            </div>    
-        </div>
-    </div>
+            </div>
+        )
+    }
+}
 
 export default Feedback
