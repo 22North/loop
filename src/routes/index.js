454efa2130985022003f220a379ab4cf0c360fc5
Awais Muzaffar <mail@awaismuzaffar.com>
2018-05-29 18:50:29 +0100

    wrap routes around switch

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,19 +9,26 @@ import {
     MyTeamDetail, 
     SignIn  
 } from "../containers"
-import { Route } from "react-router-dom"
+
+import { 
+    Route, 
+    Switch 
+} from "react-router-dom"
+
 import PrivateRoute from "../components/PrivateRoute"
 
 const Routes = () => (
     <div className="main-content">
-        <PrivateRoute exact path="/" component={Home} />
-        <PrivateRoute exact path="/objectives" component={Objectives} />
-        <PrivateRoute exact path="/objectives/create" component={CreateObjective} />
-        <PrivateRoute path="/objectives/:objectiveId" component={Objective} />
-        <PrivateRoute exact path="/feedback" component={Feedback} />
-        <PrivateRoute exact path="/my-team" component={MyTeam} />
-        <PrivateRoute path="/my-team/:userId" component={MyTeamDetail}/>
-        <Route path="/signin/" component={SignIn}/>
+        <Switch>
+            <PrivateRoute exact path="/" component={Home} />
+            <PrivateRoute exact path="/objectives" component={Objectives} />
+            <PrivateRoute exact path="/objectives/create" component={CreateObjective} />
+            <PrivateRoute path="/objectives/:objectiveId" component={Objective} />
+            <PrivateRoute exact path="/feedback" component={Feedback} />
+            <PrivateRoute exact path="/my-team" component={MyTeam} />
+            <PrivateRoute path="/my-team/:userId" component={MyTeamDetail}/>
+            <Route path="/signin/" component={SignIn}/>
+        </Switch>
     </div>
 )
 
