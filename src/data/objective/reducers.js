d3d0562761a43aa5f17e5dfb405670a336f7b7c2
Awais Muzaffar <mail@awaismuzaffar.com>
2018-06-03 10:49:41 +0100

    fixed objective view

--- src/data/objective/reducers.js
+++ src/data/objective/reducers.js
@@ -1,13 +1,20 @@
+import { GET_OBJECTIVE, CLEAR_OBJECTIVE } from './actions'
+
 const INITIAL_STATE = {
-    data: null
+    data: null,
 }
 
 export function objective(state = INITIAL_STATE, action) {
     switch (action.type) {
-        case 'GET_OBJECTIVE_SUCCESS':
+        case GET_OBJECTIVE:
             return {
                 ...state, 
-                data: action.objective
+                data: action.objective,
+            }
+        case CLEAR_OBJECTIVE:
+            return {
+                ...state, 
+                data: null,
             }
         default:
             return state
